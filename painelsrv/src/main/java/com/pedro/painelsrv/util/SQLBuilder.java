e348f0789aff4d66381ad88b73e5dea27bd56714
pedronery <DEV04@10.100.0.104>
2024-07-01 19:05:18 -0300

    Documentando a class SQLBuilder

--- painelsrv/src/main/java/com/pedro/painelsrv/util/SQLBuilder.java
+++ painelsrv/src/main/java/com/pedro/painelsrv/util/SQLBuilder.java
@@ -1,5 +1,11 @@
 package com.pedro.painelsrv.util;
 
+
+/**
+ * A classe {@code SQLBuilder} facilita a construção dinâmica de consultas SQL nativos em Java.
+ * Ela utiliza {@code StringBuilder} para montar cláusulas SQL de forma programática,
+ * permitindo adicionar campos, condições, joins, agrupamentos e ordenações de maneira intuitiva e organizada.
+ */
 public class SQLBuilder {
 
 	private StringBuilder field = new StringBuilder();
@@ -9,8 +15,13 @@ public class SQLBuilder {
 	private StringBuilder orderBy = new StringBuilder();
 	private StringBuilder from = new StringBuilder();
 
+	 /**
+     * Constrói uma nova instância de {@code SQLBuilder} para a tabela especificada.
+     *
+     * @param table o nome da tabela de onde os dados serão selecionados
+     */
 	public SQLBuilder(String table) {
-
+		
 		field.append("SELECT ");
 		from.append(" FROM ").append(table).append(" ");
 		where.append(" WHERE ");
@@ -18,6 +29,12 @@ public class SQLBuilder {
 		orderBy.append(" ORDER BY ");
 	}
 
+	 /**
+     * Adiciona um campo à cláusula {@code SELECT}.
+     *
+     * @param t o campo a ser adicionado
+     * @param <T> o tipo do campo
+     */
 	public <T> SQLBuilder appendField(T t) {
 
 		if (field.length() > 7) {
@@ -27,6 +44,12 @@ public class SQLBuilder {
 		return this;
 	}
 
+	 /**
+     * Adiciona uma condição à cláusula {@code WHERE}, incluindo automaticamente o operador {@code AND} quando necessário.
+     *
+     * @param t a condição a ser adicionada
+     * @param <T> o tipo da condição
+     */
 	public <T> SQLBuilder appendWhere(T t) {
 		if (where.length() > 7) {
 			where.append(" AND ");
@@ -35,11 +58,23 @@ public class SQLBuilder {
 		return this;
 	}
 
+	  /**
+     * Adiciona uma cláusula {@code JOIN}.
+     *
+     * @param t a cláusula {@code JOIN} a ser adicionada
+     * @param <T> o tipo da cláusula {@code JOIN}
+     */
 	public <T> SQLBuilder appendJoin(T t) {
 		join.append(" ").append(t);
 		return this;
 	}
 
+	   /**
+     * Adiciona um campo à cláusula {@code GROUP BY}.
+     *
+     * @param t o campo a ser adicionado
+     * @param <T> o tipo do campo
+     */
 	public <T> SQLBuilder appendGroupBy(T t) {
 		if (groupBy.length() > 9) {
 			groupBy.append(", ");
@@ -47,6 +82,13 @@ public class SQLBuilder {
 		groupBy.append(t);
 		return this;
 	}
+	
+	/**
+     * Adiciona um campo à cláusula {@code ORDER BY}.
+     *
+     * @param t o campo a ser adicionado
+     * @param <T> o tipo do campo
+     */
 
 	public <T> SQLBuilder appendOrderBy(T t) {
 		if (orderBy.length() > 10) {
@@ -56,27 +98,28 @@ public class SQLBuilder {
 		return this;
 	}
 
-
 	public String toWhere() {
 		return where.toString();
 	}
-
 	public String toField() {
 		return field.toString();
 	}
-
 	public String toJoin() {
 		return join.toString();
 	}
-
 	public String toGroupBy() {
 		return groupBy.toString();
 	}
-
 	public String toOrderBy() {
 		return groupBy.toString();
 	}
+	
 
+	 /**
+    * Retorna a consulta SQL final construída.
+    *
+    * @return a consulta SQL como uma {@code String}
+    */
 	@Override
 	public String toString() {
 		StringBuilder sql = new StringBuilder();
@@ -92,8 +135,14 @@ public class SQLBuilder {
 		}
 		return sql.toString();
 	}
+
+	/**
+    * Retorna a consulta SQL final construída.
+    * Este método é um alias para {@code toString()}.
+    *
+    * @return a consulta SQL como uma {@code String}
+    */
 	public String toSql() {
 		return this.toString();
 	}
-
 }
