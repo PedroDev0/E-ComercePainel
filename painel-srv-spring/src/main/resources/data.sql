85f9040c79c435e93d4aa62f6fa441a32ccbea15
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-04 23:51:47 -0300

    criando schema e inserts para nosso bd

---
+++ painel-srv-spring/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO produto (id, descricao, preco_compra, preco_venda, data_cadastro) VALUES
+(1, 'Bicicleta', 20.00, 20.00, CURRENT_TIMESTAMP ),
+(2, 'tablet', 20.00, 20.00, CURRENT_TIMESTAMP ),
+(3, 'carroça', 20.00, 20.00, CURRENT_TIMESTAMP),
+(4, 'carro', 20.00, 20.00, CURRENT_TIMESTAMP);
