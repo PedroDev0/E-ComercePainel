765e582281204a058c3a030beafe11723a512a40
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-04 23:53:58 -0300

    Criando endpoint do produto

--- painel-srv-spring/src/main/java/com/pedro/painelsrvspring/controller/ProdutoController.java
+++ painel-srv-spring/src/main/java/com/pedro/painelsrvspring/controller/ProdutoController.java
@@ -1,32 +1,26 @@
 package com.pedro.painelsrvspring.controller;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pedro.painelsrvspring.dto.ProdutoDTO;
+import com.pedro.painelsrvspring.service.ProdutoService;
 
 @RestController
 @RequestMapping("/product")
 public class ProdutoController {
 
+	@Autowired
+	ProdutoService produtoService;
+	
 	@GetMapping
 	@RequestMapping("/all-products")
-	private ResponseEntity<List<ProdutoDTO>> getAllProducts(){
-		
-		List<ProdutoDTO> dtos = new ArrayList<ProdutoDTO>();
-		
-		dtos.add(new ProdutoDTO(1, "Bicicleta",new BigDecimal("20.00"), new BigDecimal("20.00"), new Date(), null));
-		dtos.add(new ProdutoDTO(1, "tablet",new BigDecimal("20.00"), new BigDecimal("20.00"), new Date(), null));
-		dtos.add(new ProdutoDTO(1, "carroça",new BigDecimal("20.00"), new BigDecimal("20.00"), new Date(), null));
-		dtos.add(new ProdutoDTO(1, "carro",new BigDecimal("20.00"), new BigDecimal("20.00"), new Date(), null));
-		
-		return ResponseEntity.ok(dtos);
+	private ResponseEntity<List<ProdutoDTO>> getAllProducts() {
+		return ResponseEntity.ok(produtoService.getAllProducts());
 	}
 }
