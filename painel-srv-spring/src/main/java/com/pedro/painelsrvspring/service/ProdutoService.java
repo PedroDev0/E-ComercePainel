ff9e81533e5080f2cfe82ed7e8a7c4009319d4a2
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-04 23:53:37 -0300

    Criando a service de produto

---
+++ painel-srv-spring/src/main/java/com/pedro/painelsrvspring/service/ProdutoService.java
@@ -0,0 +1,26 @@
+package com.pedro.painelsrvspring.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedro.painelsrvspring.dto.ProdutoDTO;
+import com.pedro.painelsrvspring.model.Produto;
+import com.pedro.painelsrvspring.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	public List<ProdutoDTO> getAllProducts() {
+		
+		List<Produto> produtos = produtoRepository.findAll();
+		 return produtos.stream()
+                .map(ProdutoDTO::fromEntity)
+                .collect(Collectors.toList());
+	}
+}
