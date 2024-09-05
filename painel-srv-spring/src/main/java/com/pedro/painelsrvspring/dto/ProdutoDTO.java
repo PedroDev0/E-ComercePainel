e39bbf21d83e1fffc53329286b0531298eb2099d
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-04 23:53:10 -0300

    Criando Produtdto

--- painel-srv-spring/src/main/java/com/pedro/painelsrvspring/dto/ProdutoDTO.java
+++ painel-srv-spring/src/main/java/com/pedro/painelsrvspring/dto/ProdutoDTO.java
@@ -4,7 +4,17 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Set;
 
-public record ProdutoDTO(Integer id, String descricao, BigDecimal precoCompra, BigDecimal precoVenda,
-		Date dataCadastro, Set<ProdutoImagemDTO> imagens) {
+import com.pedro.painelsrvspring.model.Produto;
 
+public record ProdutoDTO(Integer id, String descricao, BigDecimal precoCompra, BigDecimal precoVenda, Date dataCadastro,
+		Set<ProdutoImagemDTO> imagens) {
+
+	public static ProdutoDTO fromEntity(Produto produto) {
+		return new ProdutoDTO(produto.getId(),
+				produto.getDescricao(), 
+				produto.getPrecoCompra(),
+				produto.getPrecoVenda(), 
+				produto.getDataCadastro(),
+				null);
+	}
 }
