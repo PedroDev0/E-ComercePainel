3c25c07c54f78f987ea4053e60e91bf17d39f94b
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-04 22:59:46 -0300

    Testando serve

---
+++ painel-srv-spring/src/test/java/com/pedro/painelsrvspring/ServeTest.java
@@ -0,0 +1,28 @@
+package com.pedro.painelsrvspring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ServeTest {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void test() {
+		
+		
+		ResponseEntity<String> response = restTemplate.getForEntity("/test", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals("ok", response.getBody());
+	}
+
+}
