54f2da3f3386f5bffc6d815d95a2394d2517181d
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-11 22:56:51 -0300

    ajustando requisição

--- painelng/src/app/modules/produto/produto.service.ts
+++ painelng/src/app/modules/produto/produto.service.ts
@@ -3,26 +3,29 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { API_URL } from 'src/environments/environment.development';
 import Produto from 'src/app/core/model/produto.model';
+import { HttpParamsService } from 'src/app/shared/services/http-params.service';
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private httpParamsService: HttpParamsService) { }
 
   getProtudo(pk: number): Observable<Produto> {
-    return this.http.get<Produto>(API_URL + '/product/' + pk);
+    return this.http.get<Produto>(API_URL + '/products/' + pk);
   }
   getListProduto(form: any): Observable<any[]> {
-    return this.http.get<any[]>(API_URL + '/product?' + new URLSearchParams(form).toString());
+  
+    const params = this.httpParamsService.toHttpParams(form);
+    return this.http.get<any[]>(API_URL + '/products/all',{ params});
   }
   update(entity: Produto): Observable<Produto> {
-    return this.http.put<Produto>(API_URL + "/product/update", entity);
+    return this.http.put<Produto>(API_URL + "/products/update", entity);
   }
   create(entity: Produto): Observable<Produto> {
-    return this.http.post<Produto>(API_URL + "/product/create", entity);
+    return this.http.post<Produto>(API_URL + "/products/create", entity);
   }
   delete(pk: number): Observable<boolean> {
-    return this.http.delete<boolean>(API_URL + "/product/delete/" + pk);
+    return this.http.delete<boolean>(API_URL + "/products/delete/" + pk);
   }
 }
