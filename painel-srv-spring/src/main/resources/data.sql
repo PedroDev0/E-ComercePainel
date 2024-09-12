94616f29f70237706becde95d98d248b98ef9e5a
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-11 21:47:09 -0300

    incluindo inserts do ProdutoImagem

--- painel-srv-spring/src/main/resources/data.sql
+++ painel-srv-spring/src/main/resources/data.sql
@@ -3,3 +3,11 @@ INSERT INTO produto (id, descricao, preco_compra, preco_venda, data_cadastro) VA
 (2, 'tablet', 20.00, 20.00, CURRENT_TIMESTAMP ),
 (3, 'carroça', 20.00, 20.00, CURRENT_TIMESTAMP),
 (4, 'carro', 20.00, 20.00, CURRENT_TIMESTAMP);
+
+INSERT INTO produto_imagem (id, produto_id, principal, uri_imagem) VALUES
+(1, 1, true, 'bicicleta1.jpg'),  
+(2, 1, false, 'bicicleta2.jpg'), 
+(3, 2, true, 'tablet1.jpg'),     -- Imagem principal para o Tablet
+(4, 3, true, 'carroca1.jpg'),    -- Imagem principal para a Carroça
+(5, 4, true, 'carro1.jpg'),      -- Imagem principal para o Carro
+(6, 4, false, 'carro2.jpg'); 
