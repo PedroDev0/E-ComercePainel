bb1c2d81370ab92cfdf8f77a1d562141ab845198
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-11 23:24:34 -0300

    removendo imports excessivos

--- painel-srv-spring/src/test/java/com/pedro/painelsrvspring/ServeTest.java
+++ painel-srv-spring/src/test/java/com/pedro/painelsrvspring/ServeTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ServeTest {
