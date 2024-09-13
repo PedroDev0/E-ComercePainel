403b448b673060d126641d1926aecb866af14465
pedronery <DEV04@10.100.0.104>
2024-09-13 12:01:48 -0300

    Adicionando consulta por id

--- painel-srv-spring/src/main/java/com/pedro/painelsrvspring/controller/ProdutoController.java
+++ painel-srv-spring/src/main/java/com/pedro/painelsrvspring/controller/ProdutoController.java
@@ -1,13 +1,13 @@
 package com.pedro.painelsrvspring.controller;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,11 +27,16 @@ public class ProdutoController {
 	private ResponseEntity<List<ProdutoDTO>> getAllProducts( 
 			@RequestParam(value = "id", required = false) Integer id,
             @RequestParam(value = "descricao", required = false) String descricao,
-            @RequestParam(value = "dataDe", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataDe,
-            @RequestParam(value = "dataAte", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataAte,
+            @RequestParam(value = "dataDe", required = false)  Date dataDe,
+            @RequestParam(value = "dataAte", required = false)  Date dataAte,
             @RequestParam(value = "precoVenda", required = false) BigDecimal precoVenda,
             @RequestParam(value = "precoCompra", required = false) BigDecimal precoCompra) {
-		return ResponseEntity.ok(produtoService.getAllProducts());
+		return ResponseEntity.ok(produtoService.getAllProducts(id,descricao,dataDe,dataAte,precoVenda,precoCompra));
 	}
 
+	@GetMapping("/{id}")
+	private ResponseEntity<ProdutoDTO> getById(@PathVariable Integer id){
+		
+		return ResponseEntity.ok(produtoService.getById(id)) ;
+	}
 }
