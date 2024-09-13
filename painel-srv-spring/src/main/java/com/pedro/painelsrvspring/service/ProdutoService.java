85e70fd95e2c56359f7b962a276cd292b0bc1d14
pedronery <DEV04@10.100.0.104>
2024-09-13 13:50:27 -0300

    incluindo o update na service

--- painel-srv-spring/src/main/java/com/pedro/painelsrvspring/service/ProdutoService.java
+++ painel-srv-spring/src/main/java/com/pedro/painelsrvspring/service/ProdutoService.java
@@ -3,14 +3,18 @@ package com.pedro.painelsrvspring.service;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.pedro.painelsrvspring.dto.ProdutoDTO;
 import com.pedro.painelsrvspring.mapper.ProdutoMapper;
 import com.pedro.painelsrvspring.model.Produto;
+import com.pedro.painelsrvspring.repository.ProdutoImagemRepository;
 import com.pedro.painelsrvspring.repository.ProdutoRepository;
 
 @Service
@@ -18,6 +22,9 @@ public class ProdutoService {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private ProdutoImagemRepository prodImagemRepository;
 
 	public List<ProdutoDTO> getAllProducts(Integer id, String descricao, Date dataDe, Date dataAte,
 			BigDecimal precoVenda, BigDecimal precoCompra) {
@@ -27,9 +34,24 @@ public class ProdutoService {
 		return produtos.stream().map(ProdutoMapper::toDTO).collect(Collectors.toList());
 	}
 
-	public ProdutoDTO getById(Integer id) {
-		
+	public ProdutoDTO findById(Integer id) {
+
 		return ProdutoMapper.toDTO(produtoRepository.findById(id).get());
+	}
+
+	public ResponseEntity<ProdutoDTO> update(ProdutoDTO dto) {
+
+		Optional<Produto> produtoExist = produtoRepository.findById(dto.id());
+		Produto produtoUpdate = ProdutoMapper.toEntity(dto);
+
+		if (produtoExist.isPresent()) {
+			produtoRepository.save(produtoUpdate);
+			prodImagemRepository.saveAll(produtoUpdate.getImagens());
+
+			return new ResponseEntity<>(new ProdutoDTO(produtoUpdate), HttpStatus.OK);
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 
 }
