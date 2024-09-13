d3383b8d874fcea8eb3c4312002d7d35d2552ef9
pedronery <DEV04@10.100.0.104>
2024-09-13 13:50:41 -0300

    Mapeando o put no endpoint

--- painel-srv-spring/src/main/java/com/pedro/painelsrvspring/controller/ProdutoController.java
+++ painel-srv-spring/src/main/java/com/pedro/painelsrvspring/controller/ProdutoController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,21 +24,27 @@ public class ProdutoController {
 	@Autowired
 	ProdutoService produtoService;
 
-
 	@GetMapping("/all")
-	private ResponseEntity<List<ProdutoDTO>> getAllProducts( 
-			@RequestParam(value = "id", required = false) Integer id,
-            @RequestParam(value = "descricao", required = false) String descricao,
-            @RequestParam(value = "dataDe", required = false)  Date dataDe,
-            @RequestParam(value = "dataAte", required = false)  Date dataAte,
-            @RequestParam(value = "precoVenda", required = false) BigDecimal precoVenda,
-            @RequestParam(value = "precoCompra", required = false) BigDecimal precoCompra) {
-		return ResponseEntity.ok(produtoService.getAllProducts(id,descricao,dataDe,dataAte,precoVenda,precoCompra));
+	private ResponseEntity<List<ProdutoDTO>> getAllProducts(@RequestParam(value = "id", required = false) Integer id,
+			@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "dataDe", required = false) Date dataDe,
+			@RequestParam(value = "dataAte", required = false) Date dataAte,
+			@RequestParam(value = "precoVenda", required = false) BigDecimal precoVenda,
+			@RequestParam(value = "precoCompra", required = false) BigDecimal precoCompra) {
+		return ResponseEntity
+				.ok(produtoService.getAllProducts(id, descricao, dataDe, dataAte, precoVenda, precoCompra));
 	}
 
 	@GetMapping("/{id}")
-	private ResponseEntity<ProdutoDTO> getById(@PathVariable Integer id){
+	private ResponseEntity<ProdutoDTO> getById(@PathVariable Integer id) {
+
+		return ResponseEntity.ok(produtoService.findById(id));
+	}
+
+	@PutMapping("/update")
+	private ResponseEntity<ProdutoDTO> update(@RequestBody ProdutoDTO dto) {
 		
-		return ResponseEntity.ok(produtoService.getById(id)) ;
+		return produtoService.update(dto);
+
 	}
 }
