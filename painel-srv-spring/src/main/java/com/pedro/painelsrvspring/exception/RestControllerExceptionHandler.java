d58a15fd983184a70c29899214a7c832091ee859
Pedro <Pedro@DESKTOP-57L26G4>
2024-09-16 22:23:03 -0300

    Mapeando exception caso de erro ao deletar

--- painel-srv-spring/src/main/java/com/pedro/painelsrvspring/exception/RestControllerExceptionHandler.java
+++ painel-srv-spring/src/main/java/com/pedro/painelsrvspring/exception/RestControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.pedro.painelsrvspring.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +13,9 @@ public class RestControllerExceptionHandler {
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<Void> handleEntityNotFoundException() {
 		return ResponseEntity.notFound().build();
+	}
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<Void> handleIllegalArgumentException() {
+		return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	}
 }
